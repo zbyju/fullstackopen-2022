2764030f4dc2dbeebe05547a8040e01ae8faeecc
zbyju <zbynek.jurica@gmail.com>
2022-09-07 18:56:04 +0300

    Implement step 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import Header from './Header'
+import FeedbackButtons from './FeedbackButtons'
+import Statistics from './Statistics'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <FeedbackButtons onGood={incrementGood} onNeutral={incrementNeutral} onBad={incrementBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ part1/unicafe/src/Statistics.js
@@ -0,0 +1,12 @@
+const Statistics = ({good, neutral, bad}) => {
+    return (
+        <>
+            <h1>Statistics</h1>
+            <p>Good - {good}</p>
+            <p>Neutral - {neutral}</p>
+            <p>Bad - {bad}</p>
+        </>
+    )
+}
+
+export default Statistics;
