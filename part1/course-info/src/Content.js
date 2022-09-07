f2b28e0a1e7277ac408386e0a6aa61541ea58776
zbyju <zbynek.jurica@gmail.com>
2022-09-07 17:17:30 +0300

    Implement step 1

---
+++ part1/course-info/src/Content.js
@@ -0,0 +1,15 @@
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => (
+    <>
+        <p>
+            {part1} {exercises1}
+        </p>
+        <p>
+            {part2} {exercises2}
+        </p>
+        <p>
+            {part3} {exercises3}
+        </p>
+    </>
+)
+
+export default Content;
---
+++ part1/course-info/src/Total.js
@@ -0,0 +1,5 @@
+const Total = ({ total }) => (
+    <p>Number of exercises {total}</p>
+)
+
+export default Total;
