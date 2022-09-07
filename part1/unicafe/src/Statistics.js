d22569dd87198b9f0522df6ce5366e2cfa00b5cd
zbyju <zbynek.jurica@gmail.com>
2022-09-07 19:04:44 +0300

    Implement step 2

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,10 +1,25 @@
 const Statistics = ({good, neutral, bad}) => {
+    const all = good + neutral + bad
+
+    const calcAverage = () => {
+        if(all === 0) return 0
+        return (good - bad) / all
+    }
+
+    const calcPositive = () => {
+        if(all === 0) return 0
+        return 100 * good / all
+    }
+
     return (
         <>
             <h1>Statistics</h1>
             <p>Good - {good}</p>
             <p>Neutral - {neutral}</p>
             <p>Bad - {bad}</p>
+            <p>All: {good + neutral + bad}</p>
+            <p>Average: {calcAverage()}</p>
+            <p>Positive: {calcPositive()}%</p>
         </>
     )
 }
