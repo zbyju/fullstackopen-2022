55e01017fa9fc70546b195b34e561cff856b400f
zbyju <zbynek.jurica@gmail.com>
2022-09-12 17:16:18 +0300

    Implement step 2, 3

--- part2/countries/src/CountryList.js
+++ part2/countries/src/CountryList.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import Country from "./Country";
+
 const CountryList = ({ countries }) => {
+  const [showCountries, setShowCountries] = useState(
+    countries.map((country) => ({
+      ...country,
+      show: false,
+    }))
+  );
+
+  function handleShowClick(name) {
+    const newShowCountries = showCountries.map((country) => {
+      if (country.name.common === name) {
+        return { ...country, show: !country.show };
+      }
+      return country;
+    });
+    setShowCountries(newShowCountries);
+  }
+
   return (
     <>
-      {countries.map((country) => (
-        <p key={country.name.common}>{country.name.common}</p>
+      {showCountries.map((country) => (
+        <div key={country.name.common}>
+          <br />
+          <span>{country.name.common} </span>
+          <button onClick={() => handleShowClick(country.name.common)}>
+            {country.show ? "Hide" : "Show"}
+          </button>
+          {country.show && <Country country={country} />}
+        </div>
       ))}
     </>
   );
