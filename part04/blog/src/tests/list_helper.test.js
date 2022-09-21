5ea862fa005471b82518148f7cd7e53ce7fb0766
zbyju <zbynek.jurica@gmail.com>
2022-09-21 17:04:59 +0300

    Add favoriteBlog function

--- part04/blog/src/tests/list_helper.test.js
+++ part04/blog/src/tests/list_helper.test.js
@@ -1,5 +1,11 @@
 const listHelper = require("../utils/list_helper");
 
+const blog1 = { title: "t1", author: "a1", url: "u1", likes: 3 };
+const blog2 = { title: "t2", author: "a2", url: "u2", likes: 10 };
+const blog3 = { title: "t3", author: "a3", url: "u3", likes: 5 };
+const blog4 = { title: "t4", author: "a4", url: "u4", likes: 1 };
+const blog5 = { title: "t5", author: "a5", url: "u5", likes: 0 };
+
 describe("dummy", () => {
   test("dummy returns one", () => {
     const blogs = [];
@@ -10,12 +16,6 @@ describe("dummy", () => {
 });
 
 describe("totalLikes", () => {
-  const blog1 = { title: "t1", author: "a1", url: "u1", likes: 3 };
-  const blog2 = { title: "t2", author: "a2", url: "u2", likes: 10 };
-  const blog3 = { title: "t3", author: "a3", url: "u3", likes: 5 };
-  const blog4 = { title: "t4", author: "a4", url: "u4", likes: 1 };
-  const blog5 = { title: "t5", author: "a5", url: "u5", likes: 0 };
-
   test("one blog should be equal to the amount of the likes", () => {
     const res = listHelper.totalLikes([blog1]);
     expect(res).toBe(blog1.likes);
@@ -30,5 +30,25 @@ describe("totalLikes", () => {
 
   test("empty array should be zero", () => {
     expect(listHelper.totalLikes([])).toBe(0);
+  });
+});
+
+describe("favoriteBlog", () => {
+  test("one blog should return that blog", () => {
+    const res = listHelper.favoriteBlog([blog1]);
+    expect(res).toEqual(blog1);
+
+    const res0 = listHelper.favoriteBlog([blog5]);
+    expect(res0).toEqual(blog5);
+  });
+
+  test("array of blogs should find the most liked one", () => {
+    const res = listHelper.favoriteBlog([blog1, blog2, blog3]);
+    expect(res).toEqual(blog2);
+  });
+
+  test("empty array should return falsy", () => {
+    const res = listHelper.favoriteBlog([]);
+    expect(res).toBeFalsy();
   });
 });
--- part04/blog/src/utils/list_helper.js
+++ part04/blog/src/utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined;
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite;
+  }, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
