4c0303ea711084ac3a4ccb834c7c022baaf1d76b
zbyju <zbynek.jurica@gmail.com>
2022-09-24 15:27:06 +0300

    Add test for checking id exists

--- part04/blog/src/tests/api/blog.test.js
+++ part04/blog/src/tests/api/blog.test.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const { response } = require("../../app");
 const app = require("../../app");
 const Blog = require("../../models/blog");
 const helper = require("./test_helper");
@@ -17,13 +18,25 @@ beforeAll(async () => {
 
 describe("after initial save", () => {
   test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("2 blogs are returned", async () => {
     const response = await api.get("/api/blogs");
 
     expect(response.body).toHaveLength(helper.initialBlogs.length);
   });
 
-  test("2 blogs are returned", async () => {
-    await api.get("/api/blogs");
+  test("the id property is defined", async () => {
+    const response = await api.get("/api/blogs");
+    for (const blog of response.body) {
+      console.log(blog);
+      expect(blog.id).toBeDefined();
+      expect(blog._id).toBeFalsy();
+    }
   });
 });
 
