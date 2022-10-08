64afb9fc88e44b95b063658c2e1e01794816fd24
zbyju <zbynek.jurica@gmail.com>
2022-10-08 22:16:54 +0300

    Add eslint

--- part05/bloglist-frontend/src/setupProxy.js
+++ part05/bloglist-frontend/src/setupProxy.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-undef
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+// eslint-disable-next-line no-undef
 module.exports = function (app) {
   app.use(
     "/api",
