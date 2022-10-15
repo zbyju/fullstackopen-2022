2cf245e808bbe9f099a2aa32e97826f11fd895a0
zbyju <zbynek.jurica@gmail.com>
2022-10-15 17:33:47 +0300

    Move anecdote list to its own component

--- part06/redux-anecdotes/src/App.js
+++ part06/redux-anecdotes/src/App.js
@@ -1,29 +1,12 @@
-import { useSelector, useDispatch } from "react-redux";
-import { addVote } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(addVote(id));
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
   );
 };
